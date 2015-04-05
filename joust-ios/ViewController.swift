2f40ea143e663d31da35a1ef59a1422cdf08134a
vaidehi <rohank85@gmail.com>
2015-04-05 00:44:44 -0500

    Initial Commit

---
+++ joust-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  joust-ios
+//
+//  Created by Rohan Kulkarni on 4/5/15.
+//  Copyright (c) 2015 gat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
